4de72a75140fa9fea5bcc77a69d90ab6778cc672
Akash <akashbahadursingh5@gmail.com>
2025-02-18 08:57:03 +0530

    changes done for responsive nav bar and starting showing data from backend

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,8 +1,9 @@
 import library from "../../assets/library.png";
+import { Link } from "react-router-dom";
 const Hero = () => {
   return (
-    <div className="h-[75vh] flex">
-      <div className="w-full lg:w-3/6 flex flex-col items-center  lg:items-start justify-center">
+    <div className="h-[75vh] flex flex-col md:flex-row items-center justify-center">
+      <div className="w-full mb-12 md:mb-0 lg:w-3/6 flex flex-col items-center  lg:items-start justify-center">
         <h1 className="text-4xl lg:text-6xl font-semibold text-yellow-100 text-center lg:text-left">
           something something book
         </h1>
@@ -11,9 +12,9 @@ const Hero = () => {
           skjdfnkadfkanf sdnflasflk
         </p>
         <div className="mt-8">
-          <button className="text-yellow-100 text-xl lg:text-2xl font-semibold border border-yellow-100 px-10 py-3 hover:bg-zinc-800 rounded-full">
+          <Link to="/all-book" className="text-yellow-100 text-xl lg:text-2xl font-semibold border border-yellow-100 px-10 py-3 hover:bg-zinc-800 rounded-full">
             Discover books
-          </button>
+          </Link>
         </div>
       </div>
       <div className="w-full lg:w-3/6 h-auto lg:h-[100%] flex items-center justify-center"><img src={library} alt="library" /></div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import Hero from "../components/home/Hero";
+import RecentlyAdded from "../components/home/RecentlyAdded";
 const Home = () => {
   return (
     <div className="bg-zinc-900 text-white px-10 py-8">
       <Hero />
+      <RecentlyAdded />
     </div>
   );
 };
